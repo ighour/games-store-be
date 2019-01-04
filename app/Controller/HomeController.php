160a19a4c2e90e2eafc3090e92549fec84d72c8f
Ighour <ti.celio.rodrigues@gmail.com>
2019-01-04 12:37:01 +0000

    Updated HomeController to check server alive.

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -2,26 +2,8 @@
 
 namespace App\Controller;
 
-use \App\DAO\User;
-
-class HomeController {
+class HomeController extends Controller {
   public function index(){
-    try{
-      $userDAO = new User();
-
-      $users = $userDAO->fetchAll();
-    }
-    catch(Exception $exception){
-      die();
-    }
-    
-    foreach($users as $user){
-      echo "ID: " . $user->id . "<br/>";
-      echo "Username: " . $user->username . "<br/>";
-      echo "Email: " . $user->email . "<br/>";
-      echo "Password: " . $user->password . "<br/>";
-      echo "Role: " . $user->role . "<br/>";
-      echo "</br>";
-    }
+    return $this->respondOk("Server is alive!");
   }
 }
