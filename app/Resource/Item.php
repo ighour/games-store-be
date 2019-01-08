6ce5ee187acf9d945f80f2676d90ea6c2886551c
Ighour <ti.celio.rodrigues@gmail.com>
2019-01-08 17:09:03 +0000

    Added Item return owner name

--- app/Resource/Item.php
+++ app/Resource/Item.php
@@ -2,9 +2,14 @@
 
 namespace App\Resource;
 
+use \App\DAO\User as UserDAO;
+
 class Item extends Resource {
   public function element($element)
   {
+    $dao = new UserDAO();
+    $user = $dao->fetchById($element->user_id);
+
     return [
       'id' => (int) $element->id,
       'name' => $element->name,
@@ -13,7 +18,8 @@ class Item extends Resource {
       'amount' => (double) $element->amount,
       'item_category_id' => (int) $element->item_category_id,
       'user_id' => (int) $element->user_id,
-      'image' => is_null($element->image) ? null : __SERVER__ . '/storage' . '/games' . '/' . $element->image
+      'image' => is_null($element->image) ? null : __SERVER__ . '/storage' . '/games' . '/' . $element->image,
+      'relation_user' => !is_null($user) ? $user->email : 'Not Provided'
     ];
   }
 }
